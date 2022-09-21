511c43b08d576534958e161baa857f39b7f10e52
Hind <hind.nayhi@gmail.com>
2022-09-21 13:23:42 -0600

    Tried to use ressources routes for user auth, but it didn't work out, so it's commented out for now.

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,16 @@ Rails.application.routes.draw do
 
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
+  
+  # resources :users, only: [:new, :create]
+
   get '/register' => 'users#new'
   post '/users' => 'users#create'
 
   # these routes are for showing users a login form, logging them in, and logging them out.
+  
+  # resources :sessions, only: [:new, :create, :destroy]
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
