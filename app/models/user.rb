3d7deeae8991e3643fd5fd39eb518d6dd1e4dbc0
Hind <hind.nayhi@gmail.com>
2022-09-26 09:57:48 -0600

    Implement validations for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, 
+    presence: true,
+    confirmation: { case_sensitive: false },
+    uniqueness: true
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
 end
