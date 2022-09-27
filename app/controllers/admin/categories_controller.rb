b20bb673193e4100ed9fc4530a746d81dd147069
Hind <hind.nayhi@gmail.com>
2022-09-27 14:36:03 -0600

    Fix bug when uploading an image for product.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USERNAME"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
-  
+
   def index
     @categories = Category.order(id: :desc).all
   end
@@ -21,7 +21,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   private
-    def product_params
+    def category_params
       params.require(:category).permit(
         :name,
       )
